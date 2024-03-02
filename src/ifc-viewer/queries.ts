cd7b1042e96f18cacf7f441e66b60ccfbf32f92f
Mads Holten Rasmussen <mads.h.rasmussen@gmail.com>
2024-03-02 20:43:51 +0100

    added rdf and queries

--- src/ifc-viewer/queries.ts
+++ src/ifc-viewer/queries.ts
@@ -20,6 +20,26 @@ export const queries: Query[] = [
       },
       highlightColor: "red",
       highlightResults: true
+    },
+    {
+      id: "listWalls",
+      query: `PREFIX bot: <https://w3id.org/bot#> 
+      PREFIX schema: <http://schema.org/> 
+      PREFIX rec: <http://purl.org/ontology/rec/core#> 
+      
+      SELECT ?s ?lat ?lng
+      WHERE {
+        ?s a bot:Building ;
+           schema:geo [
+             schema:latitude ?lat ;
+             schema:longitude ?lng 
+           ]
+      }`,
+      postProcessing: (res: any): any[] => {
+        return res.results.bindings.map(b => {
+          return {iri: iriToGlobalId(b.s.value), lat: b.lat.value, lng: b.lng.value};
+        });
+      },
     }
 ];
 
